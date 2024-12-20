97f184ce18d1c11d77cdc64c1eb19e62ad9c9696
Ujjwal Pr <ujjwalprakash5@gmail.com>
2024-12-20 10:27:32 -0800

    jenkins

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "AWS region"
+  default = "us-east-2"
+}
+
+variable "aws_type" {
+  description = "AWS ec2 type"
+  default = "t3.medium"
+}
+
+variable "aws_ami" {
+  description = "AWS ami"
+  default = "ami-022661f8a4a1b91cf"
+}
