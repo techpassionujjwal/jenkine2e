9232366a77601f85e354ba0cfd68a5f12c84c6dc
Ujjwal Prakash <40907485+techpassionujjwal@users.noreply.github.com>
2024-12-20 21:55:27 -0800

    Update main.tf

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   ami           = var.ami
   instance_type = var.instance_type
-  key_name      = "jenkinspem.pem" 
+  key_name      = "jenkinspem" 
   tags = {
     Name = var.instance_name
   }
